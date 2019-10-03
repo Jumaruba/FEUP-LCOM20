ca47246228f129e8f2e5b3b949f9adb4fcec3d5c
jumaruba <juliane.marbayashi@gmail.com>
2019-10-03 14:46:56 +0100

    modifications

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -32,20 +32,26 @@ int main(int argc, char *argv[]) {
 
 
 int (timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  
+  //Checks if timer variable is within desired range[0,2]
+  if (timer>2 || timer<0){
+    //printf("ERR: timer_get_conf --timer** timer variable out of range \n");
+    return 1; 
+  }
+
+  //allocates memory for status word
   uint8_t *st = (uint8_t*) malloc(sizeof(st));
   if(st == NULL){
-    printf("COULD NOT ALLOCATE MEMORY\n");
+    //printf("COULD NOT ALLOCATE MEMORY\n");
     return 1;
   } 
-  
+  //gets status word from timer
   if (timer_get_conf(timer, st) != 0){
-    printf("ERR: timer_test_read_config --timer_get_conf** Impossible to read config \n"); 
+    //printf("ERR: timer_test_read_config --timer_get_conf** Impossible to read config \n"); 
     return 2; 
   } 
-
+  //Checks if information was displayed properly(no errors returned)
   if(timer_display_conf(timer, *st, field) != 0){
-    printf("ERR: timer_test_read_config --timer_display_conf** Impossible to display config \n"); 
+    //printf("ERR: timer_test_read_config --timer_display_conf** Impossible to display config \n"); 
     return 3; 
   } 
 
@@ -53,9 +59,9 @@ int (timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 }
 
 int (timer_test_time_base)(uint8_t timer, uint32_t freq) {
-
+  //Only calls timer_set_frequency() (commentary really necessary? =D)
   if(timer_set_frequency(timer, freq) != 0){
-    printf("ERR: timer_test_time_base --timer_set_frequency** Impossible to set frequency \n"); 
+    //printf("ERR: timer_test_time_base --timer_set_frequency** Impossible to set frequency \n"); 
     return 1; 
   }
   
@@ -63,42 +69,47 @@ int (timer_test_time_base)(uint8_t timer, uint32_t freq) {
 }
 
 int (timer_test_int)(uint8_t time) {
- 
+if(time < 0){
+  //printf("ERR: timer_test_int --time** error from negative value");
+  return 7;
+}
+int time_freq = time * 60; //Standard frequency = 60Hz
 int ipc_status;	
 message msg;		
 int r;  
-uint8_t irq_set; 		//mask: for timer 0 set the bit 0 to one, due hook_id  = 0
-
+uint8_t irq_set; 		//mask: Since hook_id=0,sets first bit to 1 for TIMER_0
+//Sets irq_set(hook_id) bit to 1
 if (timer_subscribe_int(&irq_set)!= 0){ 
-	printf("ERR: timer_test_int --timer_subscribe_int** impossible to subscribe \n"); 
+	//printf("ERR: timer_test_int --timer_subscribe_int** impossible to subscribe \n"); 
 	return 1; 
 } 
 
-while( counter < time*60) { 
-     if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
-         printf("driver_receive failed with: %d", r);
-        continue;
+while( counter < time_freq) { 
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+      //printf("driver_receive failed with: %d", r);
+      continue;
     }
     if (is_ipc_notify(ipc_status)) { 
-        switch (_ENDPOINT_P(msg.m_source)) {
-            case HARDWARE: 				
-                if (msg.m_notify.interrupts & irq_set) { 
-                  timer_int_handler(); 
-                    if(counter %60 == 0){
-                			timer_print_elapsed_time();
-			               }           
-                }
-                break;
-            default:
-                break; 	
-            }
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: 				
+          if (msg.m_notify.interrupts & irq_set) { 
+            //to do if interrupt is detected
+            timer_int_handler(); 
+            if(counter %60 == 0){
+              timer_print_elapsed_time();
+            }           
+          }
+        break;
+        default:
+          break; 	
+      }
     } else { 
-        
-    }
+        continue;
+      }
 }
 	if (timer_unsubscribe_int() != 0){
-			printf("ERR: timer_test_int --timer_unsubscribe_int** impossible to unsubscribe \n"); 
-			return 2; 
+		//printf("ERR: timer_test_int --timer_unsubscribe_int** impossible to unsubscribe \n"); 
+		return 2; 
 	}
 
 	return 0;
--- lab2/timer.c
+++ lab2/timer.c
@@ -13,17 +13,27 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   uint8_t timerStatus;          //stores value for the status
 
 
+  //explanation 
+  //value(16bits) = 1193182(21bits)/x( n bits)
+  //if x < 19 we will have 21-n bits to the value.
+  //Then, we have that the minimum value for the frequence is 19. 
+  if (freq < 19){
+    //printf("ERR: timer_set_frequency --freq** freq out of range\n");
+    return 7;
+  }
+
   //checking possible invalid values for timer
   if (timer> 2 || timer<0){
-    printf("ERR: timer_set_frequency --timer** error sending information to control register \n");
+    //printf("ERR: timer_set_frequency --timer** error sending information to control register \n");
     return 1; 
   }
 
   //getting the configuration for timer
   if(timer_get_conf(timer, &timerStatus)!= OK){
-    printf("ERR: timer_set_frequency --status** error calling timer_get_conf");
+    //printf("ERR: timer_set_frequency --status** error calling timer_get_conf");
     return 2;
   }
+
 
   //calculating initial value for the timer
   value = TIMER_FREQ/freq; 
@@ -48,19 +58,19 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
 
   //sending controlWorld to the timer control register for configuration
   if (sys_outb(TIMER_CTRL, controlWord) != OK){
-    printf("ERR: timer_set_frequency --sys_outb** error sending information to control register \n "); 
+    //printf("ERR: timer_set_frequency --sys_outb** error sending information to control register \n "); 
       return 3; 
   }  
   //loading timer with value's lsb obtained by using util_get_LSB
   util_get_LSB(value,&temp); 
   if(sys_outb(TIMER_0+timer, temp) != OK){
-    printf("ERR: timer_set_frequency --sys_inb** error reading information from timer register \n");
+    //printf("ERR: timer_set_frequency --sys_inb** error reading information from timer register \n");
       return 4; 
   }
   //loading timer with value's msb obtained by using util_get_MSB
   util_get_MSB(value, &temp); 
   if(sys_outb(TIMER_0+timer, temp) != OK){
-    printf("ERR: timer_set_frequency --sys_inb** error reading information from timer register \n");
+    //printf("ERR: timer_set_frequency --sys_inb** error reading information from timer register \n");
       return 5; 
   }
 
@@ -73,13 +83,14 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
 int (timer_subscribe_int)(uint8_t *bit_no) {
   
   *bit_no = BIT(hook_id); 
-
+  //sets hook_id bit to 1
   if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id) != OK){
-    printf("ERR: timer_subscribe_int --sys_irqsetpolicy** error executing\n"); 
+    //printf("ERR: timer_subscribe_int --sys_irqsetpolicy** error executing\n"); 
     return 1;  
   }
 
-  /*//if add this code, it get's an error because irqsetpolicy already set the bit to 1
+  /*//if this code snipet gets uncommented, the compiler returns an error 
+  because irqsetpolicy() have already set the bit to 1
     if(sys_irqenable(&hook_id) != OK){
     printf("ERR: timer_subscribe_int --sys_irqenable* error executing\n"); 
     return 2;
@@ -93,12 +104,12 @@ int (timer_subscribe_int)(uint8_t *bit_no) {
 int (timer_unsubscribe_int)() {
  
   if(sys_irqdisable(&hook_id)!=OK){
-    printf("ERR: timer_unsubscribe_int --sys_irqenable* error executing\n"); 
+    //printf("ERR: timer_unsubscribe_int --sys_irqenable* error executing\n"); 
     return 1; 
   } 
 
   if ( sys_irqrmpolicy(&hook_id)!= OK){
-    printf("ERR: timer_unsubscribe_int --sys_irqenable* error executing\n"); 
+    //printf("ERR: timer_unsubscribe_int --sys_irqenable* error executing\n"); 
     return 2;
 
   }
@@ -116,9 +127,9 @@ void (timer_int_handler)() {
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
-  //getting errros out of range
+  //checks if timer variable is within desired range[0,2]
   if (timer>2 || timer<0){
-    printf("ERR: timer_get_conf --timer** timer variable out of range \n");
+    //printf("ERR: timer_get_conf --timer** timer variable out of range \n");
     return 1; 
   }
 
@@ -126,19 +137,17 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   //sending information to the control register
   //selecting count, because when it's set when it's zero
   if(sys_outb(TIMER_CTRL ,TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer)) != OK){
-    printf("ERR: timer_get_conf --sys_outb** error sending information to control register \n"); 
+    //printf("ERR: timer_get_conf --sys_outb** error sending information to control register \n"); 
     return 2; 
   }
 
   //READBACK reading status
   //getting information from the timer register
-  uint32_t temp; 
-  if(sys_inb(TIMER_0+timer,&temp) != OK){
-    printf("ERR: timer_get_conf --sys_inb** error reading information from timer register");
-    return 3; 
-  } 
-
-  *st = (uint8_t) temp; 
+  
+  if(util_sys_inb(TIMER_0 + timer,st) != 0){
+    //printf("ERR: timer_get_conf --util_sys_inb** error reading information from timer register");
+    return 3;
+  }
 
   return 0; 
 }
@@ -153,41 +162,58 @@ int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field fiel
   uint8_t timer_output;
   uint8_t initMask = 0x30,modeMask = 0x0E;
   union timer_status_field_val fieldVal;
-
+  //Checking if timer variable is in range [0,2]
+  if (timer>2 || timer<0){
+    printf("ERR: timer_get_conf --timer** timer variable out of range \n");
+    return 7; 
+  }
+  //Checking if READBACK is read properly and gets status word
   if (timer_get_conf(timer,&st) != OK){
     printf("ERR: timer_display_conf --timer_get_conf** error reading the timer configuration \n"); 
       return 1; 
   } 
-  //Get timer characteristics by apllying a specific mask to isolate each spec
+  //Get timer characteristics by apllying a specific mask to the status word
   timer_base = (st & BIT(0));
   timer_init = (st & initMask) >> 4;
   timer_mode = (st & modeMask) >> 1;
   timer_nullcount = (st & BIT(6)) >> 6;
   timer_output = (st & BIT(7)) >> 7;
 
-  //Dealing with bit 4 that must be ignored at times.
+  //Dealing with bit 4 that must be ignored at times(timer_mode cannot exceed 5)
   if(timer_mode == 7 || timer_mode == 6) timer_mode -= 4;
-  //timer_print_config();
-
+  printf("\n");
+  printf("-------------Timer %u -------------\n",timer);
+  printf("------------Status : %.8s\n",to_binary(8,st) );
+  //Display timer config based in which info is requested by the user
   switch(field){
     case tsf_all:
+      printf("------------Output : %u\n",timer_output);
+      printf("--------Null Count : %u\n",timer_nullcount);
+      printf("------Counter init : %u\n",timer_init);
+      printf("---Programmed mode : %u\n",timer_mode);
+      printf("-----Counting base : %u\n",timer_base);
       fieldVal.byte = st;
       timer_print_config(timer,tsf_all,fieldVal);
       break;
     case tsf_initial:
+      printf("------Counter init : %u\n",timer_init);
       fieldVal.in_mode = timer_init;
       timer_print_config(timer,tsf_initial,fieldVal);
       break;
     case tsf_mode:
+      printf("---Programmed mode : %u\n", timer_mode);
       fieldVal.count_mode = timer_mode;
       timer_print_config(timer,tsf_mode,fieldVal);
       break;
     case tsf_base:
+      printf("-----Counting base : %u\n",timer_base );
       fieldVal.bcd = timer_base;
       timer_print_config(timer,tsf_base,fieldVal);
       break;
     default:
-      printf("ERR: INVALID STATUS----TERMINATING NOW----\n");
+      //Since enum elements correspond to integers,integers out of range may be
+      //passed as the function's arguments
+      printf("ERR: INVALID FIELD VARIABLE----TERMINATING NOW----\n");
       return 2;
       break;
   }
